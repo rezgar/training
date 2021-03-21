3d13b7d337da8dd3121192f3f44274d7ab557e06
Rezgar Cadro <rezgar.cadro@gmail.com>
2021-03-21 17:00:38 +0200

    WIP

--- k8s/basic-web-app/server/routes/index.js
+++ k8s/basic-web-app/server/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var request_counter = 0;
+
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
+  if (request_counter++ >= 5)
+    throw 'Too many requests';
 
   res.json({
     value_1: "foo",
